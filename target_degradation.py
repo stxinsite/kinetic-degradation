5482dd1cf899e5da1b1a34d5f31b2dbe0a0b01e2
Daniel Park <daniel.park@roivant.com>
2021-11-15 16:21:47 -0500

    cases were refactored

--- target_degradation.py
+++ target_degradation.py
@@ -8,13 +8,6 @@ plt.rcParams["axes.titlesize"] = 20
 plt.rcParams["figure.figsize"] = (12,8)
 
 """
-This script reproduces Bartlett et al. (2013) Supplementary Figures 1b and 1c.
-
-Given initial values, we solve for target protein degradation relative to initial baseline amount
-at various concentrations of PROTAC.
-"""
-
-"""
 PLOT OPTIMAL BPD CONCENTRATION OVER TIME
 """
 Prop_Target_Deg_Grid = np.load('saved_objects/Prop_Target_Deg_Grid.npy')
@@ -40,81 +33,4 @@ ax = Target_deg_df.plot(
     figsize = (12, 8)
 )
 plt.savefig('plots/Optimal_BPD_vs_Time')
-plt.show()
-
-
-"""
-BARTLETT SUPPLEMENTARY FIGURE 1 (c)
-"""
-y0 = np.array([100 * params['Vec'] / 1000, BPD_ic, T, E3, BPD_T, BPD_E3, Ternary] + Ternary_Ubs)
-t = np.arange(start = 0, stop = 48 + 1, step = 2)
-
-results = kp.calc_concentrations(params = params, times = t, y0 = y0, max_step = 0.001)
-assert np.all(results.y >= 0), f"Results from solve_ivp() at initial value [BPD_ec] = {conc} are not all non-negative."
-
-results_df = kp.dataframe_concentrations(results)
-
-T_total = results_df.filter(regex = '(.*T)|(Ternary.*)').sum(axis = 1)
-Target_deg = T_total / T_total[0] * 100
-
-Target_deg_df = pd.DataFrame({'t': t, 'Target_deg': Target_deg})
-
-ax = Target_deg_df.plot(
-    x = 't',
-    xlabel = 'Time (h)',
-    y = 'Target_deg',
-    ylabel = '% Baseline Target Protein',
-    kind = 'line',
-    xlim = (t.min(), t.max()),
-    ylim = (0, 120),
-    xticks = np.arange(start = t.min(), stop = t.max() + 1, step = 6),
-    fontsize = 20,
-    title='Percent baseline Target protein over time\n at [$BPD_{ec}$] = 100 nM using BTK PROTAC parameters',
-    legend = False,
-    figsize = (12, 8)
-)
-plt.savefig("plots/Target_Deg_BPD=100nM.png")
-plt.show()
-
-"""
-BARTLETT SUPPLEMENTARY FIGURE 1 (b)
-"""
-Conc_BPD_ec_arr = np.logspace(base = 10.0, start = -1, stop = 5, num = 50)
-Target_deg_arr = np.empty((len(Conc_BPD_ec_arr), 2))
-
-# Takes ~100s for 20 concentrations, 2 time points each
-for count, conc in enumerate(Conc_BPD_ec_arr):
-    y0 = np.array([conc * Vec / 1000, BPD_ic, T, E3, BPD_T, BPD_E3, Ternary] + Ternary_Ubs)
-    t = np.array([0, 24])
-
-    results = kp.calc_concentrations(times = t, y0 = y0, max_step = 0.001)
-    assert np.all(results.y >= 0), f"Results from solve_ivp() at initial value [BPD_ec] = {conc} are not all non-negative."
-
-    results_df = kp.dataframe_concentrations(results)
-
-    T_total = results_df.filter(regex = '(.*T)|(Ternary.*)').sum(axis = 1)
-
-    Target_deg_arr[count] = np.array([conc, T_total.values[1] / T * 100])
-
-    if (count + 1) % 10 == 0:
-        progress = (count + 1) / len(Conc_BPD_ec_arr) * 100
-        print("Progress: " + str(progress))
-
-Target_deg_df = pd.DataFrame(Target_deg_arr, columns = ['Conc_BPD_ec', 'Target_deg'])
-
-ax = Target_deg_df.plot(
-    x = 'Conc_BPD_ec',
-    xlabel = 'BPD Concentration (nM)',
-    y = 'Target_deg',
-    ylabel = '% Baseline Target Protein',
-    kind = 'line',
-    xlim = (Conc_BPD_ec_arr.min(), Conc_BPD_ec_arr.max()),
-    ylim = (0, 120),
-    fontsize = 20,
-    logx = True,
-    title='Percent baseline Target protein at 24 hours using BTK PROTAC parameters',
-    legend = False,
-    figsize = (12, 8)
-)
-plt.savefig('plots/Target_Deg_t=24H.png')
 plt.show()
