897cd2861c44fb6df02e0d076af24e00a815c405
Daniel Park <daniel.park@roivant.com>
2021-12-14 16:42:55 -0500

    small initial BPD concentration

--- bin/run_kinetic_model.py
+++ bin/run_kinetic_model.py
@@ -14,36 +14,50 @@ if __name__ == '__main__':
     test_id = '&'.join(protac_ids)
     test_id = test_id.replace(" ", "")
 
-
     """Fixed initial intracellular BPD concentration
     over a range of time points.
     """
-    t_eval = np.linspace(0, 6, num=240)
-    initial_BPD_concs = [0.1]
+    # t_eval = np.linspace(0, 6, num=240)
+    # initial_BPD_concs = [0.1]
+    #
+    # result = kt.run_kinetic_model(
+    #     config_files=config_files,
+    #     protac_IDs=protac_ids,
+    #     t_eval=t_eval,
+    #     initial_BPD_ic_concs=initial_BPD_concs,
+    #     return_only_final_state=False
+    # )
+    #
+    # result.to_csv(f"./saved_objects/{test_id}_fix_BPD_vary_time_DEG.csv", index=False)
+
+    """Fixed initial extracellular BPD concentration
+    over a range of time points.
+    """
+    t_eval = np.linspace(0, 24)
+    initial_BPD_ec_conc = 0.0005  # uM
 
     result = kt.run_kinetic_model(
         config_files=config_files,
         protac_IDs=protac_ids,
         t_eval=t_eval,
-        initial_BPD_ec_concs=initial_BPD_concs,
+        initial_BPD_ec_concs=initial_BPD_ec_conc,
         return_only_final_state=False
     )
 
-    result.to_csv(f"./saved_objects/{test_id}_fix_BPD_vary_time_DEG.csv", index=False)
+    result.to_csv(f"./saved_objects/{test_id}_fix_BPD_ec={initial_BPD_ec_conc}_vary_time_DEG.csv", index=False)
 
-
-    """Various initial intracellular BPD concentrations
-    at a fixed time point.
+    """Vary initial extracellular BPD concentrations and
+    solve to a fixed time point.
     """
-    t_eval = np.linspace(0, 6)
-    initial_BPD_concs = np.logspace(base=10.0, start=-4, stop=2, num=50)  # various initial concentrations of BPD_ec (uM)
-
-    result = kt.run_kinetic_model(
-        config_files=config_files,
-        protac_IDs=protac_ids,
-        t_eval=t_eval,
-        initial_BPD_ec_concs=initial_BPD_concs,
-        return_only_final_state=True
-    )
-
-    result.to_csv(f"./saved_objects/{test_id}_vary_BPD_fix_time_DEG.csv", index=False)
+    # t_eval = 6  # hours
+    # initial_BPD_ec_concs = np.logspace(base=10.0, start=-4, stop=2)  # uM
+    #
+    # result = kt.run_kinetic_model(
+    #     config_files=config_files,
+    #     protac_IDs=protac_ids,
+    #     t_eval=t_eval,
+    #     initial_BPD_ec_concs=initial_BPD_ec_concs,
+    #     return_only_final_state=True
+    # )
+    #
+    # result.to_csv(f"./saved_objects/{test_id}_vary_BPD_ec_fix_time_DEG.csv", index=False)
