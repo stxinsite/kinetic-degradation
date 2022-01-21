26737166f1422835a5c23e2bee12aa73d6f8df42
Daniel Park <daniel.park@roivant.com>
2022-01-21 10:57:27 -0500

    split simulation script into deg vs protac and deg vs time

--- bin/deg_vs_time.py
+++ bin/deg_vs_time.py
@@ -15,7 +15,7 @@ if __name__ == '__main__':
     """Fixed initial intracellular BPD concentration
     over a range of time points.
     """
-    t_eval = np.linspace(0, 12, num=100)
+    t_eval = np.linspace(0, 1, num=100)
     initial_BPD_conc = 0.001
 
     result = kt.run_kinetic_model(
@@ -25,19 +25,4 @@ if __name__ == '__main__':
         initial_BPD_ec_concs=initial_BPD_conc,
         return_only_final_state=False
     )
-    result.to_csv(f"./saved_objects/{test_id}_BPD_ec={initial_BPD_conc}_DEG.csv", index=False)
-
-    """Vary initial extracellular BPD concentrations and
-    solve to a fixed time point.
-    """
-    t_eval = 6  # hours
-    initial_BPD_ec_concs = np.logspace(base=10.0, start=-4, stop=2)  # uM
-
-    result = kt.run_kinetic_model(
-        config_files=config_files,
-        protac_IDs=protac_ids,
-        t_eval=t_eval,
-        initial_BPD_ec_concs=initial_BPD_ec_concs,
-        return_only_final_state=True
-    )
-    result.to_csv(f"./saved_objects/{test_id}_t={t_eval}_DEG.csv", index=False)
+    result.to_csv(f"./saved_objects/{test_id}_bpd_ec={initial_BPD_conc}_t={np.max(t_eval)}_DEG.csv", index=False)
